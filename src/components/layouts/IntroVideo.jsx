a0aa07ef30c5a55d295499d8715d8b24e67dc1a6
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-09-05 20:15:35 +0530

    added video

--- src/components/layouts/IntroVideo.jsx
+++ src/components/layouts/IntroVideo.jsx
@@ -7,7 +7,6 @@ export default function IntroVideo() {
       <section className="text-gray-400 bg-gray-900 body-font relative flex items-center justify-center py-5 md:py-10">
         <video
           className="w-[90%]  md:w-[70%]  shadow-xl border border-gray-500 outline-none"
-          // autoPlay
           controls
           controlsList="nodownload nofullscreen noremoteplayback"
           disablePictureInPicture
